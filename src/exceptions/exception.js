819b4a49ab9a2c39189de4106d339091b808de29
ItsVuong <itsvuong0806@gmail.com>
2024-08-06 00:42:50 +0700

    Add Http exception object

---
+++ src/exceptions/exception.js
@@ -0,0 +1,12 @@
+class HttpException extends Error {
+    constructor(status, message, error){
+        super();
+        this.status = status;
+        this.message = message;
+        this.error = error;
+    }
+}
+
+module.exports = {
+    HttpException: HttpException
+}
